f2e995fdb8f52660219dc089a227dae9dcbfc4e4
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-03-24 10:19:15 -0300

    [Pages] SignIn Page Complete

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,105 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
-const SignIn: React.FC = () => <h1>SignIn</h1>;
+import * as Yup from 'yup';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import { FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
+
+import Logo from '../../components/Logo';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import Background from '../../components/Background';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+import backgroundImage from '../../assets/backgroundImageOne.jpg';
+
+import {
+  Content,
+  Container,
+  LeftContainer,
+  RightContainer,
+  CenterContainer,
+} from './style';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  const history = useHistory();
+  const { addToast } = useToast();
+  const { signIn } = useAuth();
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Please, insert your e-mail.')
+            .email('Invalid e-mail.'),
+          password: Yup.string().required('Please, insert your password.'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        history.push('/dashboard');
+        addToast({ type: 'success', title: 'Welcome!' });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        } else {
+          addToast({
+            type: 'error',
+            title: 'Error in Authentication!',
+            description: 'Incorrect e-mail/password combination.',
+          });
+        }
+      }
+    },
+    [signIn, addToast, history],
+  );
+
+  return (
+    <Container>
+      <Background imagePath={backgroundImage} />
+      <Content>
+        <LeftContainer>
+          <Link to="/">
+            <FiArrowLeft />
+          </Link>
+        </LeftContainer>
+        <CenterContainer>
+          <Logo />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <Input name="email" icon={FiMail} placeholder="E-mail..." />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Password..."
+              autoComplete="on"
+            />
+            <Button type="submit">Enter</Button>
+          </Form>
+        </CenterContainer>
+        <RightContainer>
+          <Link to="forgot-password">Forgot your Password?</Link>
+        </RightContainer>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignIn;
---
+++ src/pages/SignIn/style.ts
@@ -0,0 +1,93 @@
+import { shade } from 'polished';
+import styled, { keyframes } from 'styled-components';
+
+export const Container = styled.main`
+  height: 100vh;
+  display: flex;
+  overflow: hidden;
+  align-items: stretch;
+`;
+
+export const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  max-width: 700px;
+`;
+
+export const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  } to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const RightContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  animation: ${appearFromLeft} 1s;
+
+  margin-right: 48px;
+
+  a {
+    color: #336820;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#336820')};
+    }
+  }
+`;
+
+export const LeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  animation: ${appearFromLeft} 1s;
+
+  > a {
+    color: #336820;
+    transition: color 0.2s;
+
+    svg {
+      margin-top: 8px;
+      margin-left: 8px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#336820')};
+    }
+  }
+`;
+
+export const CenterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 1s;
+
+  div {
+    img {
+      margin-top: 72px;
+    }
+  }
+
+  form {
+    width: 600px;
+    text-align: center;
+
+    margin-top: 24px;
+    margin-bottom: 36px;
+  }
+`;
