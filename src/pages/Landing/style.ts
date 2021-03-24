ead02dc312b1783e30286c27256eeced2eb76c35
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-03-24 09:45:34 -0300

    [Pages] Add animation in Landing Page

--- src/pages/Landing/style.ts
+++ src/pages/Landing/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.main`
   height: 100vh;
@@ -17,6 +17,26 @@ export const Content = styled.section`
   max-width: 700px;
 `;
 
+export const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  } to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translate(50px);
+  } to {
+    opacity: 1;
+    transform: translate(0);
+  }
+`;
+
 export const ButtonArea = styled.div`
   display: flex;
   margin-top: 100px;
@@ -27,6 +47,14 @@ export const ButtonArea = styled.div`
 
   > a {
     margin-right: 16px;
+  }
+
+  #signin {
+    animation: ${appearFromLeft} 1s;
+  }
+
+  #signup {
+    animation: ${appearFromRight} 1s;
   }
 
   @media only screen and (max-width: 600px) {
