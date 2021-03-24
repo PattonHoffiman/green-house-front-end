ca961608b4c1d0a49fb6ac757bc9176958b6bd0d
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-03-24 17:49:00 -0300

    [Routes] Add New Routes to APP

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,20 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Landing from '../pages/Landing';
+import ConfirmEmail from '../pages/ConfirmEmail';
+import ResetPassword from '../pages/ResetPassword';
+import ForgotPassword from '../pages/ForgotPassword';
+import MailNotification from '../pages/MailNotification';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/signin" component={SignIn} />
     <Route path="/signup" component={SignUp} />
     <Route path="/" exact component={Landing} />
+    <Route path="/confirm-email" component={ConfirmEmail} />
+    <Route path="/reset-password" component={ResetPassword} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/mail-notification" component={MailNotification} />
   </Switch>
 );
 
